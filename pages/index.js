bfa39a1e2319005e3599f89662c9b305209b634a
Mitch Langlois <mitch@mitchlanglois.com>
2019-01-30 10:55:08 -0500

    Adds some linting and styling

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,54 @@
-import fetch from 'isomorphic-unfetch'
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
-const PostLink = (props) => (
+const getPosts = () => {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js' },
+    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
+    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' },
+  ]
+}
+
+const PostLink = ({ post }) => (
   <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{ props.title }</a>
+    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+      <a>{ post.title }</a>
     </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </li>
 )
 
-const Index = (props) => (
+export default () => (
   <Layout>
-    <h1>Batman TV Shows</h1>
+    <h1>Sample Blog</h1>
     <ul>
-      {props.shows.map(({ show }) => (
-        <li key={show.id}>
-          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
+      {getPosts().map((post) => (
+        <PostLink key={post.id} post={post} />
       ))}
     </ul>
+    <style jsx>{`
+      h1 {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+    `}</style>
   </Layout>
 )
-
-Index.getInitialProps = async function() {
-  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
-  const data = await res.json()
-
-  console.log(`Show data fetched. Count: ${data.length}`)
-
-  return {
-    shows: data
-  }
-}
-
-export default Index
