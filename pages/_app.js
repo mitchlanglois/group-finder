228d14dfc8a7baa0512e29f4a5e9bc12b24fb793
Mitch Langlois <mitch@mitchlanglois.com>
2019-03-20 15:14:00 -0400

    Adds Material UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,47 @@
 import App, { Container } from 'next/app'
 import { ApolloProvider } from 'react-apollo'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import JssProvider from 'react-jss/lib/JssProvider'
+import getPageContext from '../src/getPageContext'
+
 import withApollo from '../lib/withApollo'
 import Page from '../components/Page'
 
 class MyApp extends App {
+  constructor() {
+    super()
+    this.pageContext = getPageContext()
+  }
+
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
   render() {
     const { Component, pageProps, apollo } = this.props;
 
     return (
       <Container>
         <ApolloProvider client={apollo}>
-          <Page>
-            <Component {...pageProps} />
-          </Page>
+          <JssProvider
+            registry={this.pageContext.sheetsRegistry}
+            generateClassName={this.pageContext.generateClassName}
+          >
+            <MuiThemeProvider
+              theme={this.pageContext.theme}
+              sheetsManager={this.pageContext.sheetsManager}
+            >
+              <CssBaseline />
+              <Page>
+                <Component pageContext={this.pageContext} {...pageProps} />
+              </Page>
+            </MuiThemeProvider>
+          </JssProvider>
         </ApolloProvider>
       </Container>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,41 @@
-import styled from 'styled-components'
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
+
 import SignupForm from '../components/Signup'
 import LoginForm from '../components/Login'
 import User from '../components/User'
 
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  grid-gap: 20px;
-`
-export default () => (
+const styles = theme => ({
+  gridItem: {
+    height: '100%'
+  },
+});
+
+const Index = ({ classes }) => (
   <User>
     {({ data: { me } }) => (
-      <Grid>
+      <Grid container spacing={24} alignItems="stretch">
         {me && (
           <p>{me.name}</p>
         )}
         {!me && (
           <>
-            <SignupForm />
-            <LoginForm />
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.gridItem}>
+                <SignupForm />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.gridItem}>
+                <LoginForm />
+              </Paper>
+            </Grid>
           </>
         )}
       </Grid>
     )}
   </User>
 )
+
+export default withStyles(styles)(Index)
