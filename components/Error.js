c80e5aa6152034e2dd0e84876b672aeba1ae7010
Mitch Langlois <mitch@mitchlanglois.com>
2019-04-10 15:25:59 -0400

    Removes styled components

--- components/Error.js
+++ components/Error.js
@@ -1,40 +1,24 @@
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
-
-const ErrorStyles = styled.div`
-  padding: 2rem;
-  background: white;
-  margin: 2rem 0;
-  border: 1px solid rgba(0, 0, 0, 0.05);
-  border-left: 5px solid red;
-  p {
-    margin: 0;
-    font-weight: 100;
-  }
-  strong {
-    margin-right: 1rem;
-  }
-`;
 
 const Error = ({ error }) => {
   if (!error || !error.message) return null;
   if (error.networkError && error.networkError.result && error.networkError.result.errors.length) {
     return error.networkError.result.errors.map((error, i) => (
-      <ErrorStyles key={i}>
+      <div key={i}>
         <p data-test="graphql-error">
           <strong>Shoot!</strong>
           {error.message.replace('GraphQL error: ', '')}
         </p>
-      </ErrorStyles>
+      </div>
     ));
   }
   return (
-    <ErrorStyles>
+    <div>
       <p data-test="graphql-error">
         <strong>Shoot!</strong>
         {error.message.replace('GraphQL error: ', '')}
       </p>
-    </ErrorStyles>
+    </div>
   );
 };
 
